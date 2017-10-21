6524ae46d3d6205853ff2bca27a95d6fce2a9ddb
rsalge <rossnsalge@gmail.com>
2017-10-21 15:46:20 -0500

    invokes search on enter press

--- src/components/search.js
+++ src/components/search.js
@@ -21,8 +21,9 @@ angular.module('video-player')
 
 template: `
 <div class="search-bar form-inline">
-  <input class="form-control" ng-model="query" type="text" />
-  <button class="btn" ng-click="$ctrl.searchService.search(query, $ctrl.searchResults)">
+  <form ng-submit="$ctrl.searchService.search(query, $ctrl.searchResults)" >
+    <input class="form-control" ng-model="query" type="text" /><button class="btn" >
+  </form>
     <span class="glyphicon glyphicon-search"></span>
   </button>
 </div>
